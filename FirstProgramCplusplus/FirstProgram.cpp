8be500ef98f27eec3453cd09ef4473d6fb4fd8c6
Lucas Rachid Martins <lucasrachid@hotmail.com>
2025-06-04 23:14:15 -0300

    Iniciando estudos C++

---
+++ FirstProgramCplusplus/FirstProgram.cpp
@@ -0,0 +1,24 @@
+//
+// Created by lucas on 04/06/2025.
+//
+
+#include "FirstProgram.h"
+#include <iostream>
+using namespace std;
+
+// Em c++ a função main é obrigatória! Caso contrário o programa não compilará
+// Ela é o ponto inicial do programa, para ser compilado e executado.
+
+// Os argumentos argc, e *argv, eles servem para passar argumentos para o programa
+// mas são opcionais, você só irá utilizar caso precise capturar os argumentos passados pelo terminal.
+
+// argc = quantos argumentos vieram da linha de comando.
+// argv = array com cada argumento como string.
+// argv[0] sempre é o nome do programa.
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
---
+++ FirstProgramCplusplus/FirstProgram.h
@@ -0,0 +1,16 @@
+//
+// Created by lucas on 04/06/2025.
+//
+
+#ifndef FIRSTPROGRAM_H
+#define FIRSTPROGRAM_H
+
+
+
+class FirstProgram {
+
+};
+
+
+
+#endif //FIRSTPROGRAM_H
